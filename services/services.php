36667396a63f703ad4c5286d6708d8327dbeb0f7
Pablo de la Peña <p@shedcollective.org>
2015-10-17 13:57:11 +0100

    Updated services to allow for easy app overriding

--- services/services.php
+++ services/services.php
@@ -3,7 +3,11 @@
 return array(
     'services' => array(
         'Comment' => function () {
-            return new \Nails\Comment\Library\Comment();
+            if (class_exists('\App\Comment\Library\Comment')) {
+                return new \App\Comment\Library\Comment();
+            } else {
+                return new \Nails\Comment\Library\Comment();
+            }
         }
     )
 );
